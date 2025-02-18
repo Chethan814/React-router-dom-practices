def198e7fa4602017b0eb2e802b477690f884a80
chethan <chethanchethu99339@gmail.com>
2025-02-18 08:26:06 +0530

    Add main application structure with routing and basic styles

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    // createRoutesFromElements,
+    // Route,
+} from "react-router-dom";
+
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+import Product from "./pages/ProductPages";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+// import { Children } from "react";
+
+// const routeDef = createRoutesFromElements(
+//     <Route>
+//         <Route path="/" element={<Home />} />
+//         <Route path="Product" element={<Product />} />
+
+//     </Route>
+// )
+
+// const router = createBrowserRouter(routeDef)
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootLayout />,
+        errorElement: <ErrorPage/> ,
+        children: [
+            { path: "/", element: <Home /> },
+            { path: "/contact", element: <Contact /> },
+            { path: "/product", element: <Product /> },
+        ],
+    },
+]);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
