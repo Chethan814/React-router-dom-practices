71afe74ad48258478dc84db156844191ba0d51a6
chethan <chethanchethu99339@gmail.com>
2025-02-20 08:18:45 +0530

    Add navigation button to Home component for product page redirection

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
+
+    function navHandler () {
+        navigate('/product')
+    }
+
     return (
         <>
             <h2>home</h2>
-            <p>click here to vist <Link to='/product'>product page</Link></p>
+            <p>
+                click here to vist <Link to="/product">product page</Link>
 
-            
+            </p>
+            <button onClick={navHandler}>Navigate</button>
         </>
     );
 }
