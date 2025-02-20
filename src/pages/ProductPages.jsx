7406db30cfc751e4c77b854d0f7a59fff699cee9
chethan <chethanchethu99339@gmail.com>
2025-02-20 08:19:29 +0530

    Add product list with links to individual product pages in Product component

--- src/pages/ProductPages.jsx
+++ src/pages/ProductPages.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
+
+const products = [
+    { id: "p1", title: "Product 1" },
+    { id: "p2", title: "Product 2" },
+    { id: "p3", title: "Product 3" },
+];
+
 function Product() {
     return (
         <>
             <h2>Product page</h2>
+            <ul className="list">
+                {products.map((product) => {
+                    return (
+                        <li key={product.id}>
+                            <Link to={`/product/${product.id}`}>
+                                {product.title}
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
         </>
     );
 }
